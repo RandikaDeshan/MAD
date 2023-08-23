156f633170bbef5f741bc0353c807fd9d132d108
WishwaniKarunarathna <mlawakarunarathna@students.nsbm.ac.lk>
2023-08-23 23:56:58 +0530

    homepage

---
+++ nbook/lib/Screens/home2.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Home2 extends StatefulWidget {
+  const Home2({super.key});
+
+  @override
+  State<Home2> createState() => _Home2State();
+}
+
+class _Home2State extends State<Home2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(children: const [
+        Padding(padding: EdgeInsets.only(top: 20)),
+        Post(),
+        SizedBox(
+          height: 20,
+        ),
+        Post(),
+        SizedBox(
+          height: 20,
+        ),
+        Post(),
+        SizedBox(
+          height: 20,
+        ),
+      ]),
+    );
+  }
+}
