5109d7b189934e9c98b0565cbd0ab1b3d26b4f27
RandikaDeshan <randikadesh99@gmail.com>
2023-08-27 21:33:54 +0530

    Adding Navbar

---
+++ nbook/lib/Tab/navhome.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:nbook/Screens/homepage.dart';
+
+import '../Screens/notification.dart';
+
+class NavBar extends StatelessWidget {
+  const NavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text("TRD Peiris"),
+            accountEmail: Text("trdpeiris@students.nsbm.ac.lk"),
+            currentAccountPicture: CircleAvatar(
+                child: ClipOval(
+                    child: Image.asset(
+              "assets/ieee.jpg",
+              width: 90,
+              height: 90,
+              fit: BoxFit.cover,
+            ))),
+            decoration: const BoxDecoration(
+              color: Colors.green,
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text("Home"),
+            onTap: () => Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) => const HomePage())),
+          ),
+          ListTile(
+            leading: const Icon(Icons.notifications),
+            title: const Text("Notifications"),
+            onTap: () => Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                    builder: (context) => const NotificationPage())),
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text("Settings"),
+            hoverColor: Colors.green,
+            onTap: () => HomePage(),
+          ),
+          ListTile(
+            leading: const Icon(Icons.search),
+            title: const Text("Search"),
+            onTap: () => null,
+          ),
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text("Logout"),
+            onTap: () => null,
+          ),
+        ],
+      ),
+    );
+  }
+}
