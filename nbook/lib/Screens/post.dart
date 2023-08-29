703352eae102a27798714ca4208323f6ea9428ec
WishwaniKarunarathna <mlawakarunarathna@students.nsbm.ac.lk>
2023-08-29 18:02:55 +0530

    post

--- nbook/lib/Screens/post.dart
+++ nbook/lib/Screens/post.dart
@@ -3,3 +3,18 @@ import 'package:flutter/material.dart';
 class Post extends StatefulWidget {
   const Post({super.key});
 }
+
+class _PostState extends State<Post> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: const BoxDecoration(
+      border: Border(
+          left: BorderSide.none,
+          right: BorderSide.none,
+          top: BorderSide(color: Colors.black),
+          bottom: BorderSide(color: Colors.black)),
+      color: Colors.white,
+    ));
+  }
+}
