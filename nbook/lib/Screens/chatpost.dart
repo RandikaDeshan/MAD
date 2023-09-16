d39a48d84bf481eb9008fbe18369eb71115e0d00
Nethmi9906 <98698138+Nethmi9906@users.noreply.github.com>
2023-09-16 11:00:17 +0530

    Create chatpost new file

---
+++ nbook/lib/Screens/chatpost.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Chatpost extends StatelessWidget {
+  const Chatpost({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
+      
