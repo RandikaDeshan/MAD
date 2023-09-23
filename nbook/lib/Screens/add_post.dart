4aa4c7330201287d611a4b3242738a1d24658d3f
WishwaniKarunarathna <mlawakarunarathna@students.nsbm.ac.lk>
2023-09-23 20:05:02 +0530

    add post

--- nbook/lib/Screens/add_post.dart
+++ nbook/lib/Screens/add_post.dart
@@ -1,1 +1,193 @@
+import 'dart:typed_data';
 
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:nbook/Tab/navhome.dart';
+import 'package:nbook/components/firestore_methods.dart';
+import 'package:nbook/components/user_provider.dart';
+import 'package:nbook/components/utill.dart';
+import 'package:provider/provider.dart';
+
+import '../model/user_model.dart';
+
+class PostAdd extends StatefulWidget {
+  const PostAdd({super.key});
+
+  @override
+  State<PostAdd> createState() => _PostAddState();
+}
+
+class _PostAddState extends State<PostAdd> {
+  Uint8List? _file;
+  final TextEditingController _descriptionController = TextEditingController();
+  bool _isLoading = false;
+  void postImage(
+    String uid,
+    String username,
+    String profImage,
+  ) async {
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      String res = await FirestoreMethods().uploadPost(
+          _descriptionController.text, _file!, uid, username, profImage);
+      if (res == "success") {
+        setState(() {
+          _isLoading = false;
+        });
+        showSnackBar('Posted!', context);
+        clearImage();
+      } else {
+        setState(() {
+          _isLoading = false;
+        });
+        showSnackBar(res, context);
+      }
+    } catch (e) {
+      showSnackBar(e.toString(), context);
+    }
+  }
+
+  _selectImage(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            title: const Text("Create a post"),
+            children: [
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text("Take a photo"),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(
+                    ImageSource.camera,
+                  );
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text("Choose from gallery"),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(
+                    ImageSource.gallery,
+                  );
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text("Cancel"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  void clearImage() {
+    setState(() {
+      _file = null;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _descriptionController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    User user = Provider.of<UserProvider>(context).getUser;
+    return _file == null
+        ? Scaffold(
+            drawer: const NavBar(),
+            appBar: AppBar(
+              backgroundColor: Colors.green,
+              title: const Text("NBook"),
+              actions: [
+                IconButton(onPressed: () {}, icon: const Icon(Icons.upload))
+              ],
+            ),
+            body: Center(
+                child: IconButton(
+                    onPressed: () => _selectImage(context),
+                    icon: const Icon(Icons.upload))),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.green,
+              leading: IconButton(
+                icon: const Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                ),
+                onPressed: clearImage,
+              ),
+              title: const Text("Post"),
+              actions: [
+                IconButton(
+                    onPressed: () =>
+                        postImage(user.uid, user.username, user.photoUrl),
+                    icon: const Icon(Icons.add))
+              ],
+            ),
+            body: Column(
+              children: [
+                _isLoading
+                    ? const LinearProgressIndicator()
+                    : const Padding(padding: EdgeInsets.only(top: 0)),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(user.photoUrl),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.45,
+                      child: TextField(
+                        controller: _descriptionController,
+                        decoration: const InputDecoration(
+                          hintText: "write a caption..",
+                          border: InputBorder.none,
+                        ),
+                        maxLines: 8,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 45,
+                      width: 45,
+                      child: AspectRatio(
+                        aspectRatio: 487 / 451,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                            image: MemoryImage(_file!),
+                            fit: BoxFit.fill,
+                            alignment: FractionalOffset.topCenter,
+                          )),
+                        ),
+                      ),
+                    ),
+                    const Divider(),
+                  ],
+                )
+              ],
+            ),
+          );
+  }
+}
